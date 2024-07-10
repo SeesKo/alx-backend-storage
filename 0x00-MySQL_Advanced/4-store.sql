9933ee5b08906a525c598fcd5410afe5b6464519
SeesKo <seeskosee@seeskome.com>
2024-07-10 06:43:58 +0000

    decrease item quantity

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,10 @@
+-- Create Trigger Script: decrease item quantity
+DELIMITER $$
+CREATE TRIGGER decrease_item_quantity AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END $$
+DELIMITER ;
