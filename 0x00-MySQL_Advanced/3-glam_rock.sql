ac6e9115fcbb244bd23f4956f38b967f22e76dfa
SeesKo <seeskosee@seeskome.com>
2024-07-10 21:50:28 +0000

    calculate lifespan

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Calculate lifespan in years until 2022
 SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
-    FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-    ORDER BY lifespan DESC;
+FROM metal_bands
+WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+ORDER BY lifespan DESC;
