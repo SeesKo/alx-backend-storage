fb01680e8625c65dda268e7d8693cf79472b1a0e
SeesKo <seeskosee@seeskome.com>
2024-07-10 20:26:13 +0000

    compute and store score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,26 +1,16 @@
 -- Compute and store the average student score
--- Change the delimiter to define the procedure
 DELIMITER $$
 
 -- Create the procedure
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    DECLARE total_score FLOAT DEFAULT 0;
-    DECLARE project_count INT DEFAULT 0;
     DECLARE avg_score FLOAT DEFAULT 0;
 
-    -- Calculate the total score and the number of projects for the user
-    SELECT IFNULL(SUM(score), 0), COUNT(*)
-    INTO total_score, project_count
+    -- Calculate the average score for the user
+    SELECT IFNULL(AVG(score), 0)
+    INTO avg_score
     FROM corrections
     WHERE corrections.user_id = user_id;
-
-    -- Calculate the average score
-    IF project_count > 0 THEN
-        SET avg_score = total_score / project_count;
-    ELSE
-        SET avg_score = 0;
-    END IF;
 
     -- Update the user's average score
     UPDATE users
@@ -28,5 +18,4 @@ BEGIN
     WHERE id = user_id;
 END $$
 
--- Reset the delimiter
 DELIMITER ;
