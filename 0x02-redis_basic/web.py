f8c4e65adf5b55f91f289bd0b173d76a63220d59
SeesKo <seeskome@seeskosee.com>
2024-07-17 06:09:35 -0700

    expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,55 @@
 #!/usr/bin/env python3
 """
-Caching request module
+Module provides functions for web interactions
+including caching with Redis.
 """
-import redis
+
 import requests
+import redis
+import time
 from functools import wraps
 from typing import Callable
 
+# Initialize Redis connection
+redis_instance = redis.Redis()
 
-def track_get_page(fn: Callable) -> Callable:
-    """ Decorator for get_page
+
+def count_calls(method: Callable) -> Callable:
     """
-    @wraps(fn)
+    Decorator to count how many times a function is called.
+    """
+    @wraps(method)
     def wrapper(url: str) -> str:
-        """ Wrapper that:
-            - check whether a url's data is cached
-            - tracks how many times get_page is called
-        """
-        client = redis.Redis()
-        client.incr(f'count:{url}')
-        cached_page = client.get(f'{url}')
-        if cached_page:
-            return cached_page.decode('utf-8')
-        response = fn(url)
-        client.set(f'{url}', response, 10)
-        return response
+        count_key = f"count:{url}"
+        redis_instance.incr(count_key)
+        return method(url)
     return wrapper
 
 
-@track_get_page
+def cache_result(timeout: int) -> Callable:
+    """
+    Decorator to cache the result of a function with a timeout.
+    """
+    def decorator(method: Callable) -> Callable:
+        @wraps(method)
+        def wrapper(url: str) -> str:
+            cache_key = f"cache:{url}"
+            cached_result = redis_instance.get(cache_key)
+            if cached_result:
+                return cached_result.decode('utf-8')
+            else:
+                result = method(url)
+                redis_instance.setex(cache_key, timeout, result)
+                return result
+        return wrapper
+    return decorator
+
+
+@count_calls
+@cache_result(timeout=10)
 def get_page(url: str) -> str:
-    """ Makes a http request to a given endpoint
+    """
+    Retrieve the HTML content of a given URL.
     """
     response = requests.get(url)
     return response.text
