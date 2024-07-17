07bc94a0889d4b49e5d1e5d2608be7dc653d1eae
SeesKo <seeskome@seeskosee.com>
2024-07-17 03:24:29 -0700

    recover original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,7 +6,7 @@ Redis and store data with random keys.
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -28,3 +28,30 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+        self, key: str,
+        fn: Optional[Callable] = None
+    ) -> Optional[Union[str, bytes, int, float]]:
+        """
+        Retrieve the data from Redis by key, and
+        optionally apply a conversion function.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve the data as a UTF-8 string.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieve the data as an integer.
+        """
+        return self.get(key, fn=int)
